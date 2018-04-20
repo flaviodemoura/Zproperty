742f48c558f2f469b33bc95d8477c5d205e5d32a
Flávio L. C. de Moura <flaviomoura@unb.br>
2018-04-20 17:14:51 -0300

    Fixed Zprop definition.

--- src/ZtoConfl.v
+++ src/ZtoConfl.v
@@ -6,14 +6,45 @@ Inductive refltrans {A:Type} (R: Rel A) : A -> A -> Prop :=
 | refl: forall a, (refltrans R) a a
 | rtrans: forall a b c, R a b -> refltrans R b c -> refltrans R a c.
 
-Definition Zprop {A:Type} (R: Rel A) := exists wb:A -> A, forall a b, R a b -> (refltrans R) b (wb a) -> (refltrans R) (wb a) (wb b).
+Definition Zprop {A:Type} (R: Rel A) := exists wb:A -> A, forall a b, R a b -> ((refltrans R) b (wb a) /\ (refltrans R) (wb a) (wb b)).
 
 Definition Confl {A:Type} (R: Rel A) :=
   forall a b c, (refltrans R) a b -> (refltrans R) a c -> (exists d, (refltrans R) b d /\ (refltrans R) c d).
 
+Lemma CompReflTrans {A} (R: Rel A): forall a b c, refltrans R a b -> refltrans R b c -> refltrans R a c.
+Proof.
+  Admitted.
+  
 Theorem Zprop_implies_Confl {A:Type}: forall R: Rel A, Zprop R -> Confl R.
 Proof.
   intros R HZprop.
   unfold Zprop in HZprop.
   destruct HZprop.
+  unfold Confl.
+  intros a b c Hrefl1 Hrefl2.
+  generalize dependent c.
+  induction Hrefl1.
+  - intros c Hrefl.
+    exists c. split.
+    + assumption.
+    + apply refl.
+  - intros c0 Hrefl2.
+    assert (Hreflto: refltrans R b (x a) -> refltrans R (x a) (x b)).
+    {
+      apply H; assumption.
+    }
+    assert (Hcons: refltrans R a (x b)).
+    {
+      apply CompReflTrans with (x a).
+      - apply CompReflTrans with b.
+        + apply rtrans with b.
+          * assumption.
+          * apply refl.
+        +
+      - apply H
+    }    
+    apply H in H0. Focus 2.
+    apply IHHrefl1 in Hrefl1.
+    destruct Hrefl1.
+    
 Admitted.
