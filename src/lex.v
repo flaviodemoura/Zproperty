02a0c2239be9dbb40dd64b54596adbb7269edcc2
SerialKira <l-ordo.ab.chao@hotmail.com>
2019-11-18 12:44:08 -0300

    lc_equiv_lc_at finalizado
    Uma prova direta poderia usar o comprimento do pré-termo

--- src/lex.v
+++ src/lex.v
@@ -294,51 +294,6 @@ Proof.
     + apply IHt2; assumption.
 Qed.
 
-Lemma lc_equic_lc_at: forall t, lc t <-> lc_at 0 t.
-Proof.
-  split.
-  - intro Hlc.
-    induction Hlc.
-    + simpl.
-      tauto.
-    + simpl.
-      split; assumption.
-    + simpl.
-      pick_fresh x.
-      apply notin_union in Fr.
-      destruct Fr as [Fr Hfvt1].
-      clear Hfvt1 H.
-      apply H0 in Fr.
-      unfold open in Fr.
-      apply lc_rec_open_var_rec in Fr; assumption.
-    + split.
-      * pick_fresh x.
-        apply notin_union in Fr.
-        destruct Fr as [Fr Hfvt2].
-        apply notin_union in Fr.
-        destruct Fr as [Fr Hfvt1].
-        clear Hfvt1 Hfvt2 H.
-        apply H0 in Fr.
-        unfold open in Fr.
-        apply lc_rec_open_var_rec in Fr; assumption.
-      * assumption.
-  - intro Hlc_at.
-    induction t0.
-    + simpl in Hlc_at.
-      inversion Hlc_at.
-    + apply lc_var.
-    + simpl in Hlc_at.
-      destruct Hlc_at as [Hlc_at_t0_1 Hlc_at_t0_2].
-      apply lc_app.
-      * apply IHt0_1 in Hlc_at_t0_1; assumption.
-      * apply IHt0_2 in Hlc_at_t0_2; assumption.
-    + apply lc_abs with (fv t0).
-      intros x H.
-      unfold open.
-      admit.
-    + simpl in Hlc_at.
-  Admitted.
-
 Lemma term_to_lc_at : forall t, term t -> lc_at 0 t.
 Proof.
   intros t Hterm.
@@ -631,6 +586,43 @@ Proof.
            *** assumption.
         ** simpl in Hlc.
            apply Hlc.
+Qed.
+
+Lemma lc_equiv_lc_at: forall t, lc t <-> lc_at 0 t.
+Proof.
+  split.
+  - intro Hlc.
+    induction Hlc.
+    + simpl.
+      tauto.
+    + simpl.
+      split; assumption.
+    + simpl.
+      pick_fresh x.
+      apply notin_union in Fr.
+      destruct Fr as [Fr Hfvt1].
+      clear Hfvt1 H.
+      apply H0 in Fr.
+      unfold open in Fr.
+      apply lc_rec_open_var_rec in Fr; assumption.
+    + split.
+      * pick_fresh x.
+        apply notin_union in Fr.
+        destruct Fr as [Fr Hfvt2].
+        apply notin_union in Fr.
+        destruct Fr as [Fr Hfvt1].
+        clear Hfvt1 Hfvt2 H.
+        apply H0 in Fr.
+        unfold open in Fr.
+        apply lc_rec_open_var_rec in Fr; assumption.
+      * assumption.
+  - intro Hlc_at.
+    apply term_equiv_lc_at in Hlc_at.
+    induction Hlc_at.
+    + apply lc_var.
+    + apply lc_app; assumption.
+    + apply lc_abs with L; assumption.
+    + apply lc_sub with L; assumption.
 Qed.
 
 Theorem body_lc_at: forall t, body t <-> lc_at 1 t.
